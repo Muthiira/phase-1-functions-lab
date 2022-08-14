258aa8baf2426a0a7c455559131db5e98689d724
Muthiira <calebmuchiri48@gmail.com>
2022-08-14 11:45:43 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(n) {
+	return Math.abs(n - 42);
+}
+
+function distanceFromHqInFeet(n) {
+	const distanceFromHqInFeet = distanceFromHqInBlocks(n) * 264;
+	return distanceFromHqInFeet;
+}
+
+function distanceTravelledInFeet(start, destination) {
+	if (destination > start) {
+		const travel = (destination - start) * 264;
+		return travel;
+	}
+	else
+		return (start - destination) * 264;
+
+}
+
+function calculatesFarePrice(destination, start) {
+	const travel = (destination - start) * 264;
+	if (travel <= 400) {
+		return 0;
+	}
+	else if (travel > 400 && travel <= 2000) {
+		return (travel - 400)*(0.02);
+	}
+	else if (travel > 2000 && travel <= 2500) {
+		return 25;
+	}
+	else {
+		return "cannot travel that far";
+	}
+}
